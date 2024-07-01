f6c29035d6cce491a9691b4ab0954b069d862694
CosmomediaCEO <93375686+CosmomediaCEO@users.noreply.github.com>
2024-07-01 15:59:59 +0200

    Update CleanupCommand.php

--- src/Command/CleanupCommand.php
+++ src/Command/CleanupCommand.php
@@ -15,15 +15,24 @@ namespace Sonata\NotificationBundle\Command;
 
 use Sonata\NotificationBundle\Backend\BackendInterface;
 use Sonata\NotificationBundle\Backend\QueueDispatcherInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
 
 /**
  * @final since sonata-project/notification-bundle 3.13
  */
-class CleanupCommand extends ContainerAwareCommand
+class CleanupCommand extends Command
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        parent::__construct();
+    }
+
     public function configure()
     {
         $this->setName('sonata:notification:cleanup');
@@ -46,7 +55,7 @@ class CleanupCommand extends ContainerAwareCommand
      */
     private function getBackend()
     {
-        $backend = $this->getContainer()->get('sonata.notification.backend');
+        $backend = $this->container->get('sonata.notification.backend');
 
         if ($backend instanceof QueueDispatcherInterface) {
             return $backend->getBackend(null);
